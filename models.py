035822a353b87d3f6a0273b0b4d67ea2682a4ef6
LuizaaQueiroz <luizaquiroz234@gmail.com>
2026-03-22 13:02:18 -0300

    estrutura front

--- models.py
+++ models.py
@@ -4,10 +4,14 @@ db = SQLAlchemy()
 
 class Tarefa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
+    titulo = db.Column(db.String(100))
     descricao = db.Column(db.String(200))
-    prioridade = db.Column(db.String(10))  # baixa, media, alta
-    concluida = db.Column(db.Boolean, default=False)
+    prioridade = db.Column(db.String(10))
+    concluida = db.Column(db.Boolean)
+
+    data_limite = db.Column(db.String(20))
+    categoria = db.Column(db.String(20))
+    criada_em = db.Column(db.String(30))
 
     def to_dict(self):
         return {
